ae47bb5a7dad59a3085bdb493a73d9ff1c8dec5e
Jimmy Qiu <shihao.jimmy.qiu@gmail.com>
2022-04-07 08:54:28 +1000

    Fix CI

--- priv/repo/migrations/20220406042127_create_meetings_questions.exs
+++ priv/repo/migrations/20220406042127_create_meetings_questions.exs
@@ -2,6 +2,8 @@ defmodule ElixirMelbourne.Repo.Migrations.CreateMeetingsQuestions do
   use Ecto.Migration
 
   def change do
+    drop table(:meetings_questions)
+
     create table(:meetings_questions) do
       add :content, :string, comment: "question content"
       timestamps()

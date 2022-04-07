7e270dc563a202d0deafb6708bc8192b38ff9d74
William Kurniawan <kurniawanw0194@gmail.com>
2022-04-07 21:21:32 +1000

    fix: migration

--- priv/repo/migrations/20220406042127_create_meetings_questions.exs
+++ priv/repo/migrations/20220406042127_create_meetings_questions.exs
@@ -2,10 +2,9 @@ defmodule ElixirMelbourne.Repo.Migrations.CreateMeetingsQuestions do
   use Ecto.Migration
 
   def change do
-    drop table(:meetings_questions)
-
     create table(:meetings_questions) do
       add :content, :string, comment: "question content"
+
       timestamps()
     end
   end
